f9a5aea53771bddaffc7e9c177a1fec577168ee7
Kyle Beasley <kyle.beasley17@gmail.com>
2025-09-24 20:02:11 -0500

    refactored to use the cheechstack routing library

--- app/Http/PageController.php
+++ app/Http/PageController.php
@@ -6,11 +6,13 @@ use Twig\Environment;
 use Twig\Error\Error;
 use Twig\Loader\FilesystemLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PageController
 {
     protected FilesystemLoader $loader;
     protected Environment $twig;
+
     protected function __construct() {
         $this->loader = new FilesystemLoader(dirname(__DIR__, 2) . "/templates");
         $this->twig = new Environment($this->loader, []);
@@ -19,34 +21,49 @@ class PageController
     /** Returns the home page.
      * @throws Error
      */
-    public static function home() : void
+    public static function home() : Response
     {
         $c = new PageController();
-        echo $c->twig->render("pages/home.html.twig", []);
+        $htmlContent = $c->twig->render("pages/home.html.twig", []);
+
+        return new Response($htmlContent, 200);
     }
 
     /** Returns the about page.
      * @throws Error
      */
-    public static function about() : void
+    public static function about() : Response
     {
         $c = new PageController();
-        echo $c->twig->render("pages/about.html.twig", []);
+        $htmlContent = $c->twig->render("pages/about.html.twig", []);
+
+        return new Response($htmlContent, 200);
     }
 
-    public static function hireMe() : void
+    /** Returns the hire-me page
+     *
+     * @return Response
+     * @throws Error
+     */
+    public static function hireMe() : Response
     {
         $c = new PageController();
-        echo $c->twig->render("pages/hire-me.html.twig", []);
+        $htmlContent = $c->twig->render("pages/hire-me.html.twig", []);
+
+        return new Response($htmlContent, 200);
     }
 
     /** Returns one of the error pages.
+     *
      * @param int $code
+     * @return Response
      * @throws Error
      */
-    public static function fallback(int $code = 500) : void
+    public static function fallback(int $code = 500) : Response
     {
         $c = new PageController();
-        echo $c->twig->render("error/$code.html.twig");
+        $htmlContent = $c->twig->render("error/$code.html.twig");
+
+        return new Response($htmlContent, 200);
     }
 }
--- public/index.php
+++ public/index.php
@@ -7,8 +7,10 @@ ini_set('display_startup_errors', 1);
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 use Dotenv\Dotenv;
-use \Symfony\Component\HttpFoundation\Request;
-use App\Router\Router;
+use Symfony\Component\HttpFoundation\Request;
+use Cheechstack\Routing\Router;
+use Cheechstack\Routing\Route;
+use App\Http\PageController;
 
 // Load the ENV file
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
@@ -17,13 +19,27 @@ $dotenv->load();
 // Build the request
 $request = Request::createFromGlobals();
 
-// Load the router
-$router = new Router(); // Load the router
+// Build the router object
+$router = new Router();
 
-// Process the request
-try {
-    $router->handle($request);
-} catch (\Exception $e) {
-    echo $e->getMessage();
+// Create the allowed routes
+$routes = [
+    new Route('/', "GET", [PageController::class, 'home']),
+    new Route('/about', "GET", [PageController::class, 'about']),
+    new Route('/hire-me', "GET", [PageController::class, 'hireMe']),
+];
+
+// Load the routes into the router
+$router->add($routes);
+
+// Get the Router's response
+$response = $router->handle($request);
+
+// Return the response if all went well.
+if ($response->getStatusCode() === 200) {
+    $response->send();
 }
-
+// Return error fallback otherwise
+else {
+    (PageController::fallback($response->getStatusCode()))->send();
+}
