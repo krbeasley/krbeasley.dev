ea15dff228e9ea508d58a136038440986876abfb
Kyle Beasley <kyle.beasley17@gmail.com>
2025-10-15 14:15:34 -0500

    refactored to make the service cards a reusable component

--- app/Http/PageController.php
+++ app/Http/PageController.php
@@ -17,8 +17,37 @@ class PageController extends Controller
      */
     public static function home() : Response
     {
+        $service_card_data = [
+            [
+                "card_title" => "Google Workspace Tools & Automation",
+                "card_text" => "Automate tasks and connect Google Workspace with your business tools. From setup to ongoing support, I streamline workflows that save time and boost productivity.",
+                "img_path" => "/public/images/icons/google_workspace_logo.png",
+                "img_alt" => "Google Workspace Logo",
+                "button_link" => "/projects",
+                "button_text" => "Explore Examples",
+            ],
+            [
+                "card_title" => "Full Stack Web Development",
+                "card_text" => "Custom websites and web apps built from front to back. I also provide hosting and DNS management, delivering fast, secure, and scalable solutions tailored to your business needs.",
+                "img_path" => "/public/images/icons/server.svg",
+                "img_alt" => "Server SVG Icon",
+                "button_link" => "/projects",
+                "button_text" => "View Projects",
+            ],
+            [
+                "card_title" => "Custom Software & Systems Solutions",
+                "card_text" => "From integrations to automation, I turn your ideas into working systems using the right mix of code, tools, and technology to deliver reliable, efficient results.",
+                "img_path" => "/public/images/icons/network-right.svg",
+                "img_alt" => "Network right icon",
+                "button_link" => "/projects",
+                "button_text" => "Explore the Possibilities",
+            ],
+        ];
+
         $c = new PageController();
-        $htmlContent = $c->twig->render("pages/home.html.twig", []);
+        $htmlContent = $c->twig->render("pages/home.html.twig", [
+            "service_cards" => $service_card_data,
+        ]);
 
         return new Response($htmlContent, 200);
     }
